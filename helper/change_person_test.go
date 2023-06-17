43bc6f6843d988973afcfabbd19fa145cf90e466
RageNeko26 <rneko2006@gmail.com>
2023-06-17 15:49:21 +0700

    Refactor

--- helper/change_person_test.go
+++ helper/change_person_test.go
@@ -6,15 +6,24 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var expected *Person = new(Person)
+var expected Person = Person{
+	Name: "John",
+	Nick: "Doe",
+	Age:  40,
+}
+
+// func BenchmarkCase(b *testing.B) {
+// 	for i := 0; i < b.N; i++ {
+// 		*expected = Person{
+// 			Name: "John",
+// 			Nick: "Doe",
+// 			Age:  40,
+// 		}
+// 	}
+// }
 
 func TestFirst(t *testing.T) {
 	// first case
-	*expected = Person{
-		Name: "John",
-		Nick: "Doe",
-		Age:  40,
-	}
 
 	test_person := Person{
 		Name: "Budi Setiawan",
@@ -26,6 +35,6 @@ func TestFirst(t *testing.T) {
 
 	result := ChangePerson(pointer_test)
 	t.Run("first", func(t *testing.T) {
-		assert.Equal(t, expected, result, "Result is not as expected")
+		assert.Equal(t, &expected, result, "Result is not as expected")
 	})
 }
